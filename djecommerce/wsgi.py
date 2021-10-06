b49af11e2c4ef545f5fb969f153f4df0e34cfcc1
Raj295 <51864196+Raj295@users.noreply.github.com>
2021-10-06 10:53:55 +0100

    Update wsgi.py

--- djecommerce/wsgi.py
+++ djecommerce/wsgi.py
@@ -1,7 +1,5 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-djecomme
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rce.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
 application = get_wsgi_application()
